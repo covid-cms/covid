702b8389d42bb24d41605cbbf9f8054ef62a83b8
phambinh217 <phambinh217@gmail.com>
2020-04-26 17:17:30 +0700

    Change file path

--- app/Http/Controllers/Api/Blog/CategoryController.php
+++ app/Http/Controllers/Api/Blog/CategoryController.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api\Blog;
 
 use App\Repositories\Blog\CategoryRepository;
 use App\Http\Controllers\Controller;
 use App\Format\Blog\CategoryFormat;
-use App\Http\Requests\Api\Blog\Category;
+use App\Http\Requests\Api\Blog\Category as CategoryRequest;
 use Illuminate\Http\Request;
+use App\Models\Blog\Category;
 
 class CategoryController extends Controller
 {
@@ -24,10 +25,24 @@ class CategoryController extends Controller
         return response()->json($results);
     }
 
-    public function store(Category\CreateRequest $request)
+    public function store(CategoryRequest\CreateRequest $request)
     {
         $standardizedData = $request->standardize()->all();
         $category = $this->categoryRepo->create($standardizedData);
+
+        return response()->json([
+            'error' => false,
+            'data' => [
+                'category' => $category->format(CategoryFormat::STANDARD)
+            ]
+        ]);
+    }
+
+    public function update(CategoryRequest\UpdateRequest $request, Category $category)
+    {
+        $standardizedData = $request->standardize()->all();
+
+        $this->categoryRepo->update($category, $standardizedData);
 
         return response()->json([
             'error' => false,
