bcc1efa3d761819f4747cf8f927676e21a4f83f0
phambinh217 <phambinh217@gmail.com>
2020-04-26 16:42:11 +0700

    Create BlogCatgeoryFactory.php

---
+++ database/factories/BlogCatgeoryFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Models\Blog\Category;
+use Faker\Generator as Faker;
+use Illuminate\Support\Str;
+
+$factory->define(Category::class, function (Faker $faker) {
+    $title = $faker->name;
+
+    return [
+        'title' => $title,
+        'slug' => Str::slug($title),
+        'parent_id' => 0,
+    ];
+});
