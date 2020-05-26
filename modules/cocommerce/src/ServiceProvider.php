32e4010afa8c386ce67b5b6507a26f123c3c8613
phambinh217 <phambinh217@gmail.com>
2020-05-26 20:58:54 +0700

    rebuild

---
+++ modules/cocommerce/src/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Covid\Cocommerce;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
